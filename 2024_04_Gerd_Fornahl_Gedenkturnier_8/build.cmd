c21aa8e2fbe563d73f4c3a7588ff114d7ba9383c
eskopp <skopp.erik@gmail.com>
2024-04-18 11:55:50 +0200

    build skript

---
+++ 2024_04_Gerd_Fornahl_Gedenkturnier_8/build.cmd
@@ -0,0 +1,4 @@
+pdflatex 2024_04_Gerd_Fornahl_Gedenkturnier_8
+biber 2024_04_Gerd_Fornahl_Gedenkturnier_8
+pdflatex 2024_04_Gerd_Fornahl_Gedenkturnier_8
+pdflatex 2024_04_Gerd_Fornahl_Gedenkturnier_8
